c6f5d18c24e2aa72f49e825642966594c15dfbcf
azwarbahar <azwarbahar07@gmail.com>
2023-05-16 14:50:14 +0800

    fix Edit User

--- app/src/main/java/com/azwar/androidtestcao/ui/EditUserActivity.kt
+++ app/src/main/java/com/azwar/androidtestcao/ui/EditUserActivity.kt
@@ -1,11 +1,16 @@
 package com.azwar.androidtestcao.ui
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.widget.DatePicker
 import com.azwar.androidtestcao.R
 import com.azwar.androidtestcao.databinding.ActivityEditUserBinding
 import com.azwar.androidtestcao.models.User
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class EditUserActivity : AppCompatActivity() {
 
@@ -22,12 +27,41 @@ class EditUserActivity : AppCompatActivity() {
         initData(user)
         binding.tvCancel.setOnClickListener { finish() }
 
+        binding.etDob.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+    }
+    private fun showDatePickerDialog() {
+        val c = Calendar.getInstance()
+        var year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                var dayfix = "" + dayOfMonth;
+                var monthfix = "" + monthOfYear;
+
+                if (monthOfYear < 10) {
+                    monthfix = "0" + monthfix;
+                }
+                if (dayOfMonth < 10) {
+                    dayfix = "0" + dayfix;
+                }
+                binding.etDob.setText("$dayfix/$monthfix/$year")
+
+            }, year, month, day
+        )
+
+        dpd.show()
     }
 
     private fun initData(user: User) {
-        binding.etFirstName.setText(user.firstName ?: "-")
-        binding.etLastName.setText(user.lastName ?: "-")
-        binding.etEmail.setText(user.email ?: "-")
-        binding.etDob.setText(user.dob ?: "-")
+        binding.etFirstName.setText(user.firstName ?: "")
+        binding.etLastName.setText(user.lastName ?: "")
+        binding.etEmail.setText(user.email ?: "")
+        binding.etDob.setText(user.dob ?: "")
     }
 }
