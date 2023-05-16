0f5750bda9676dea20489b2444baf66548418e0d
azwarbahar <azwarbahar07@gmail.com>
2023-05-16 13:44:57 +0800

    add Assets data.json

---
+++ app/src/main/java/com/azwar/androidtestcao/ui/EditUserActivity.kt
@@ -0,0 +1,12 @@
+package com.azwar.androidtestcao.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azwar.androidtestcao.R
+
+class EditUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user)
+    }
+}
